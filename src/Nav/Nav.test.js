575ccce7c1fcfd2401dfd433df6c92bec3899e76
Tatiana Haddad <tatianadhaddad@gmail.com>
2019-05-12 13:25:49 -0700

    updated app with all features except delete

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import Nav from './Nav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Nav />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <Nav />
+    </BrowserRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
